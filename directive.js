40694446810778657baab35d5a95a8ce2a8e71a6
Celso Trindade <celsomtrindade@hotmail.com>
2015-09-18 11:57:30 -0300

    Update directive.js
    Doesn't need to use parent element with `ID`

--- directive.js
+++ directive.js
@@ -1,27 +1,27 @@
-	.directive('affixMe', ['$timeout','$window', function($timeout, $window) {
-		return {
-			restrict: 'A',
-			link: function(scope, element) {
-				scope.width = element.prop('offsetWidth');
-				var elWidth = scope.width + 'px',
-					elChild = angular.element(element[0].querySelector(':first-child'));
-					elChild.css('width', elWidth);
-				angular.element($window).bind("scroll", function() {
-					var affixElement = document.getElementById('affix'),
-						yPosition = 0;
-					function getPosition(element) {
-						while(element) {
-							yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
-							element = element.offsetParent;
-						}
+.directive('affixMe', ['$timeout','$window', function($timeout, $window) {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			scope.width = element.prop('offsetWidth');
+			var elWidth = scope.width + 'px',
+				elChild = angular.element(element[0].querySelector(':first-child'));
+				elChild.css('width', elWidth);
+			angular.element($window).bind("scroll", function() {
+				var myElem = element[0];
+					yPosition = 0;
+				function getPosition(element) {
+					while(element) {
+						yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+						element = element.offsetParent;
 					}
-					getPosition(affixElement);
-					if (yPosition >= 0) {
-						elChild.removeClass('affix');
-					} else if ( yPosition < 0) {
-						elChild.addClass('affix');
-					};
-				});
-			}
-		};
-	}])
+				}
+				getPosition(myElem);
+				if (yPosition >= 0) {
+					elChild.removeClass('affix');
+				} else if ( yPosition < 0) {
+					elChild.addClass('affix');
+				};
+			});
+		}
+	};
+}])
